c6ca13afbba9f7307f6dd27491d97fedb16a4fa1
mawilsn <mawilsn1989@gmail.com>
2018-10-24 19:37:38 -0500

    Tabs to Panels
    I made all the tabs into panels. Also, added a readme file.

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -3,19 +3,25 @@ library(httr)
 library(future)
 library(promises)
 library(DT)
-library(magrittr)
-library(highcharter)
-library(plotly)
+# library(magrittr)
+# library(highcharter)
+# library(plotly)
+# library(shinydashboard)
+# library(reshape)
+# library(quantmod)
+# library(ggplot2)
+# library(reshape2)
+# library(scales)
 plan(multiprocess)
 
 #Starts the server which serves the data
 #Everything appears to render first on the server then sent to the UI.
 myServer<-shinyServer(function(input, output) {
-  source("functions.R")
+  source("./functions.R")
   
-
   
-
+  
+  
   
   
   #Changes year
@@ -38,7 +44,7 @@ myServer<-shinyServer(function(input, output) {
     output$df<-DT::renderDataTable({api(input$element_id,input$changeyear,input$checkbox)})
   })
   
-#example of handling checboxk
+  #example of handling checboxk
   # output$value <- renderPrint({ input$checkbox })
   
   #changes year range on the 2nd tab
@@ -48,7 +54,7 @@ myServer<-shinyServer(function(input, output) {
     # if(input$checkbox==TRUE){
     #   qry<-"https://api.census.gov/data/timeseries/healthins/sahie?get=NAME,NIC_PT,NIC_UB90,NIC_LB90,NIC_MOE,SEXCAT,NUI_PT,NUI_UB90,NUI_LB90,PCTIC_LB90,PCTIC_MOE,PCTIC_PT,PCTIC_UB90,PCTUI_PT,PCTUI_UB90,PCTUI_LB90,PCTUI_MOE,RACE_DESC,SEX_DESC,GEOCAT,GEOID,AGE_DESC,IPR_DESC&for=county&in=state:19&"
     # }else{
-      qry<-"https://api.census.gov/data/timeseries/healthins/sahie?get=NAME,NIC_PT,NIC_UB90,NIC_LB90,NIC_MOE,SEXCAT,NUI_PT,NUI_UB90,NUI_LB90,PCTIC_LB90,PCTIC_MOE,PCTIC_PT,PCTIC_UB90,PCTUI_PT,PCTUI_UB90,PCTUI_LB90,PCTUI_MOE,RACE_DESC,SEX_DESC,GEOCAT,GEOID,AGE_DESC,IPR_DESC&for=state:19&"
+    qry<-"https://api.census.gov/data/timeseries/healthins/sahie?get=NAME,NIC_PT,NIC_UB90,NIC_LB90,NIC_MOE,SEXCAT,NUI_PT,NUI_UB90,NUI_LB90,PCTIC_LB90,PCTIC_MOE,PCTIC_PT,PCTIC_UB90,PCTUI_PT,PCTUI_UB90,PCTUI_LB90,PCTUI_MOE,RACE_DESC,SEX_DESC,GEOCAT,GEOID,AGE_DESC,IPR_DESC&for=state:19&"
     # }
     qry<- paste(qry,"YEAR=",newValue,"&NIPR_PT&RACECAT&AGECAT&IPRCAT", sep="")
     print(qry)
@@ -66,14 +72,14 @@ myServer<-shinyServer(function(input, output) {
     }
     qry<- paste(qry,"YEAR=",newValue,"&NIPR_PT&RACECAT&AGECAT&IPRCAT", sep="")
     print(qry)
-      Download.File.Blocking(qry)
+    Download.File.Blocking(qry)
   })
-#This is an Async 
+  #This is an Async 
   #don't worry about it
   # output$table <- DT::renderDataTable({
   #   GET.async.df.header.first("https://api.census.gov/data/timeseries/healthins/sahie?get=NAME,NIC_PT,NIC_UB90,NIC_LB90,NIC_MOE,SEXCAT,NUI_PT,NUI_UB90,NUI_LB90,PCTIC_LB90,PCTIC_MOE,PCTIC_PT,PCTIC_UB90,PCTUI_PT,PCTUI_UB90,PCTUI_LB90,PCTUI_MOE,RACE_DESC,SEX_DESC,GEOCAT,GEOID&for=state:19&YEAR&NIPR_PT&RACECAT")
   # })
-
+  
   
   
 })
--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -1,49 +1,22 @@
 library(shiny)
+# library(shinydashboard)
+# library(reshape)
+# library(quantmod)
+# library(ggplot2)
+# library(reshape2)
+# library(scales)
 
 
 
-source("functions.R")
-
+source("./front-end/tab1.R")
 
 
 # Define UI for application that plots random distributions 
-yrs<- api_dynamic_year_list()
-sts<-api_dynamic_state_list()
+
 print(sts)
-myUI<- shinyUI(fluidPage(
-  
-  
-  #Title
-  titlePanel("Data Programing in R"),
-  #Sidebar
-  sidebarLayout(
-    sidebarPanel( "sidebar panel",
-                  #drop down for years
-                  selectInput('changeyear', label='Select Year:', choice=yrs),
-                  #drop down for states
-                  selectInput('element_id', label = 'Select one option', choices = c(sts)),
-                  #Shows counties
-                  checkboxInput("checkbox", label = "Show Counties", value = FALSE),
-                  #creates a Year range only effects second tab
-                  sliderInput("yearrange", "Year Range:",
-                              min =min(as.numeric(yrs)), max=max(as.numeric(yrs)),
-                              value = c(min(as.numeric(yrs)), max(as.numeric(yrs))),
-                              sep = "", ticks=TRUE, step=1),
-                  #save button
-                  actionButton("button", "Save All STATE Data")
 
 
-                  ),
-    #Main Panel
-    mainPanel("main panel", 
-              h1('Report'), 
-              #creates Tabs
-              tabsetPanel(type="tabs",
-                          tabPanel("dfTable",DT::dataTableOutput("df")),
-                          tabPanel("allStatestable",dataTableOutput('table')))
-              
-              
-              
-    )
-  )))
 
+
+
+myUI<- shinyUI(navbarPage("",tab1))
